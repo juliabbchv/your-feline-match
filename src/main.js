e7022bce704b74ed0ce1c07772fc76f4faa8862a
Julia Babicheva <julia.bbchv@gmail.com>
2025-01-10 15:30:33 -0800

    new commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import API from "./API.js";
+// API setup
 
-const catApi = new API();
+const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export default class CatApi {
+  constructor() {
+    this.apiKey = API_KEY;
+    this.baseURL = BASE_URL;
+  }
+  async getCats() {
+    try {
+      const response = await axios.get(
+        `${this.baseURL}breeds?api_key=${this.apiKey}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error is:", error);
+    }
+  }
+}
+
+const catApi = new CatApi();
 
 async function allBreeds() {
   try {
